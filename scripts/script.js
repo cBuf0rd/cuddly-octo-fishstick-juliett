b1b5805a30f00449b590389bce57e4a0e0391a69
cBuf0rd <156353278+cBuf0rd@users.noreply.github.com>
2024-04-17 16:16:07 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,56 @@
-
-$("#usr").keydown(function (e) {
-    let inputVal = $(this).val();
-
-    // test input value against regular expression
-    if (alphaRegex.test(inputVal)) {
-      $(this).removeClass("error").addClass("success");
-      $(this).next().text("super cool!");
-    } else {
-      $(this).removeClass("success").addClass("error");
-      $(this).next().text("ah, farts!");
+$('#convert').click(function () {
+    $("#output").animate({
+      left: '250px',
+      opacity: '0.8',
+      height: '150px',
+      width: '150px'
+    }, "slow");
+    var input = document.getElementById('usr').value.toUpperCase();
+    var output = document.getElementById('output');
+    var natoAlphabet = {
+      'A': 'Alfa',
+      'B': 'Bravo',
+      'C': 'Charlie',
+      'D': 'Delta',
+      'E': 'Echo',
+      'F': 'Foxtrot',
+      'G': 'Golf',
+      'I': 'India',
+      'H': 'Hotel',
+      'J': 'Juliett',
+      'K': 'Kilo',
+      'L': 'Lima',
+      'M': 'Mike',
+      'N': 'November',
+      'O': 'Oscar',
+      'P': 'Papa',
+      'Q': 'Quebec',
+      'R': 'Romeo',
+      'S': 'Sierra',
+      'T': 'Tango',
+      'U': 'Uniform',
+      'V': 'Victor',
+      'W': 'Whiskey',
+      'X': 'X-ray',
+      'Y': 'Yankee',
+      'Z': 'Zulu',
+    };
+    var result = '';
+    var kebabCase = document.getElementById('kcChkBox').checked;
+    for (var i=0; i < input.length; i++){
+      var letter = input[i];
+      if(natoAlphabet[letter]){
+        result += kebabCase ? natoAlphabet[letter].toLowerCase() + '-' : natoAlphabet[letter] + ' ';
+      //  result += natoAlphabet[letter] + ' ';
+      }
+      else{
+        result += letter + ' (Not in NATO alphabet) ';
+      }
     }
+      output.textContent = result;
+    });
+    
+  $('#clear').click(function () {  
+      document.getElementById('usr').value = '';
+      document.getElementById('output').textContent = '';
   });
